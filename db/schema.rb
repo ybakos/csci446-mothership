c1d39ac15bf44622730fcb848aec998eb5c88209
Yong Bakos <ybakos@humanoriented.com>
2011-04-26 14:52:55 -0600

    Adding migrated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(:version => 20110417232256) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["perishable_token"], :name => "index_users_on_perishable_token"
   add_index "users", ["role_id"], :name => "index_users_on_role_id"
 
   create_table "versions", :force => true do |t|

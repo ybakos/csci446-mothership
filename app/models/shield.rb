072617a036c6582b7d2ffc909edd7773436d4f15
Yong Bakos <ybakos@humanoriented.com>
2011-05-04 12:10:20 -0600

    Adding default shield image to integration config.

--- app/models/shield.rb
+++ app/models/shield.rb
@@ -3,9 +3,13 @@ class Shield < ActiveResource::Base
   self.site = INTEGRATION_CONFIG[Rails.env]['shield']['endpoint_url']
 
   def image_path
-  	extension = File.extname(self.photo_file_name)
-    base = INTEGRATION_CONFIG[Rails.env]['shield']['image_base_url']
-    "#{base}/#{id}/thumb#{extension}"
+  	if self.photo_file_name.nil?
+      INTEGRATION_CONFIG[Rails.env]['shield']['image_default_url']
+	else
+	  extension = File.extname(self.photo_file_name)
+	  base = INTEGRATION_CONFIG[Rails.env]['shield']['image_base_url']
+	  "#{base}/#{id}/thumb#{extension}"
+    end
   end
 
 end

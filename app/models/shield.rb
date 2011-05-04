8a1715ceb4ec25539dbdaedb914eadac187fd706
Yong Bakos <ybakos@humanoriented.com>
2011-05-04 11:36:28 -0600

    Fixing filename extension for shield photos.

--- app/models/shield.rb
+++ app/models/shield.rb
@@ -3,8 +3,9 @@ class Shield < ActiveResource::Base
   self.site = INTEGRATION_CONFIG[Rails.env]['shield']['endpoint_url']
 
   def image_path
+  	extension = File.extname(self.photo_file_name)
     base = INTEGRATION_CONFIG[Rails.env]['shield']['image_base_url']
-    "#{base}/#{id}/thumb.png"
+    "#{base}/#{id}/thumb#{extension}"
   end
 
 end

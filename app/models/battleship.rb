97f1cbeffe221f9cc6d6546ffed6eb9e0435a3ca
Daniel Hathorn <dmhathorn@gmail.com>
2011-05-08 21:43:32 -0600

    added view link

--- app/models/battleship.rb
+++ app/models/battleship.rb
@@ -1,12 +1,13 @@
-class Shield < ActiveResource::Base
-  self.site = INTEGRATION_CONFIG[Rails.env]['shield']['endpoint_url']
+class Battleship < ActiveResource::Base
+
+  self.site = INTEGRATION_CONFIG[Rails.env]['battleship']['endpoint_url']
 
   def image_path
   	if self.photo_file_name.nil?
-      INTEGRATION_CONFIG[Rails.env]['shield']['image_default_url']
+      INTEGRATION_CONFIG[Rails.env]['battleship']['image_default_url']
 	else
 	  extension = File.extname(self.photo_file_name)
-	  base = INTEGRATION_CONFIG[Rails.env]['shield']['image_base_url']
+	  base = INTEGRATION_CONFIG[Rails.env]['battleship']['image_base_url']
 	  "#{base}/#{id}/thumb#{extension}"
     end
   end

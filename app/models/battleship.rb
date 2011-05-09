c22da569b4f4308cfcb86738a93fe132e8cafa16
Daniel Hathorn <dmhathorn@gmail.com>
2011-05-08 21:15:55 -0600

    added battleship ActiveResource model

---
+++ app/models/battleship.rb
@@ -0,0 +1,14 @@
+class Shield < ActiveResource::Base
+  self.site = INTEGRATION_CONFIG[Rails.env]['shield']['endpoint_url']
+
+  def image_path
+  	if self.photo_file_name.nil?
+      INTEGRATION_CONFIG[Rails.env]['shield']['image_default_url']
+	else
+	  extension = File.extname(self.photo_file_name)
+	  base = INTEGRATION_CONFIG[Rails.env]['shield']['image_base_url']
+	  "#{base}/#{id}/thumb#{extension}"
+    end
+  end
+
+end

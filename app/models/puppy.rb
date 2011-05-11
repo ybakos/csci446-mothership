89fa066036a4b5d7a6703d5aa334e61c5c4a7901
Michael Rose <elementation@gmail.com>
2011-05-11 10:13:23 -0600

    Integrating puppies into mothership

---
+++ app/models/puppy.rb
@@ -0,0 +1,15 @@
+class Puppy < ActiveResource::Base
+
+  self.site = INTEGRATION_CONFIG[Rails.env]['puppy']['endpoint_url']
+
+  def image_path
+  	if self.photo_file_name.nil?
+      INTEGRATION_CONFIG[Rails.env]['puppy']['image_default_url']
+	else
+	  extension = File.extname(self.photo_file_name)
+	  base = INTEGRATION_CONFIG[Rails.env]['puppy']['image_base_url']
+	  "#{base}/#{id}/thumb#{extension}"
+    end
+  end
+
+end
